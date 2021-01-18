7629f1a2463bb3c319c953398da5e9fd95b9054d
gusadolfo123 <gusadolfo123@hotmail.com>
2021-01-18 16:24:52 -0500

    ajd

--- js/app.js
+++ js/app.js
@@ -10,10 +10,25 @@ document.addEventListener("DOMContentLoaded", function (event) {
     const itemsBottom = document.getElementsByClassName("login-bottom")[0];
     itemsBottom.classList.add("hide-login-bottom");
 
-    const itemLogo = document.querySelector(".login-body > img");
-    itemLogo.classList.add("hide-login-body");
+    const itemLogo = document.getElementsByClassName("login-body--img")[0];
+    itemLogo.classList.remove("login-body--img");
+    itemLogo.classList.add("hide-login-body--img");
+
+    const itemImage = document.getElementsByClassName("batman-img")[0];
+    itemImage.classList.remove("batman-img");
+    itemImage.classList.add("down-batman-img");
 
     const itemTitle = document.getElementsByClassName("login-body--title")[0];
     itemTitle.classList.add("hide-login-body--title");
+
+    const itemCity = document.getElementsByClassName("city-img")[0];
+    itemCity.classList.add("show-city-img");
+
+    const itemRegister = document.getElementsByClassName("register-bottom")[0];
+    // itemRegister.classList.remove("register-bottom");
+    // itemRegister.classList.add("show-register-panel");
+
+    
+
   });
 });
