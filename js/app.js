d8e110bb610f04a07526a9d27edeb9080ae92631
Gustavo Moreno <ECM1238E@claro.com.co>
2021-01-18 07:29:58 -0500

    ajustes

--- js/app.js
+++ js/app.js
@@ -2,11 +2,18 @@ document.addEventListener("DOMContentLoaded", function (event) {
   const login = document.getElementById("login");
   const signup = document.getElementById("signup");
 
-  login?.addEventListener("click", function (ev) {
+  login.addEventListener("click", function (ev) {
     console.log("clicked: login");
   });
 
-  signup?.addEventListener("click", function (ev) {
-    console.log("clicked signup");
+  signup.addEventListener("click", function (ev) {
+    const itemsBottom = document.getElementsByClassName("login-bottom")[0];
+    itemsBottom.classList.add("hide-login-bottom");
+
+    const itemLogo = document.querySelector(".login-body > img");
+    itemLogo.classList.add("hide-login-body");
+
+    const itemTitle = document.getElementsByClassName("login-body--title")[0];
+    itemTitle.classList.add("hide-login-body--title");
   });
 });
